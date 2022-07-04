411af1ed05307f5591d91eca026022f886aec40a
ChupaChui <vladchuiko@gmail.com>
2022-07-04 14:05:32 +0300

    Lesson 3 -- main.py Coffee task - coffee.py

---
+++ test2.py
@@ -0,0 +1,11 @@
+# Hi
+print('Hi')
+# howdy_man?
+print('howdy', 'man', sep='_', end='?\n\n')
+
+# print O
+print('#########\n', '#\t\t#\n'*3, '#########\n', sep='')
+
+# print H
+print('#\t\t#\n'*2, '#########\n', '#\t\t#\n'*2, sep='')
+
